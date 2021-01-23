9adfd5a2ed66f979ac331819d45a9e434780d083
xQuarz <stefanie-schreiner@gmx.de>
2021-01-23 22:46:24 +0100

    Add scripts to create and delete gke cluster

---
+++ bin/cluster-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+####################################################
+#
+# Setup gke cluster
+#
+####################################################
+
+set -e
+
+$GKE=`gcloud container clusters list | grep ma`
+if [[ -z $GKE ]]
+then
+	gcloud container clusters create ma --zone europe-west4-a --node-locations europe-west4-a --preemptible --num-nodes=5 --enable-autoscaling --max-nodes=5 --min-nodes=0 --machine-type=e2-medium
+fi
