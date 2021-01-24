ac066d7bf91bdc42748f288167f1951f2222d1a4
xQuarz <stefanie-schreiner@gmx.de>
2021-01-24 07:40:37 +0100

    Scaled cluster up for keycloak and showcase

--- bin/cluster-build.sh
+++ bin/cluster-build.sh
@@ -6,14 +6,14 @@
 #
 ####################################################
 
-set -e
+#set -e
 
 CLUSTER_NAME="ma"
 
 GKE=`gcloud container clusters list | grep $CLUSTER_NAME`
 if [[ -z $GKE ]]
 then
-	gcloud container clusters create $CLUSTER_NAME --zone europe-west4-a --node-locations europe-west4-a --preemptible --num-nodes=5 --enable-autoscaling --max-nodes=5 --min-nodes=0 --machine-type=e2-medium
+	gcloud container clusters create $CLUSTER_NAME --zone europe-west4-a --node-locations europe-west4-a --preemptible --num-nodes=6 --enable-autoscaling --max-nodes=6 --min-nodes=0 --machine-type=e2-medium
 fi
 
 gcloud container clusters get-credentials $CLUSTER_NAME
