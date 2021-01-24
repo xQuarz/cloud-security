c7ba05856045fd33ab9cac8bbf7de6d1038317bf
xQuarz <stefanie-schreiner@gmx.de>
2021-01-24 04:37:53 +0100

    Get credentials for cluster ma

--- bin/cluster-build.sh
+++ bin/cluster-build.sh
@@ -6,10 +6,14 @@
 #
 ####################################################
 
-#set -e
+set -e
 
-GKE=`gcloud container clusters list | grep ma`
+CLUSTER_NAME="ma"
+
+GKE=`gcloud container clusters list | grep $CLUSTER_NAME`
 if [[ -z $GKE ]]
 then
-	gcloud container clusters create ma --zone europe-west4-a --node-locations europe-west4-a --preemptible --num-nodes=5 --enable-autoscaling --max-nodes=5 --min-nodes=0 --machine-type=e2-medium
+	gcloud container clusters create $CLUSTER_NAME --zone europe-west4-a --node-locations europe-west4-a --preemptible --num-nodes=5 --enable-autoscaling --max-nodes=5 --min-nodes=0 --machine-type=e2-medium
 fi
+
+gcloud container clusters get-credentials $CLUSTER_NAME
